be20b47aeaacda3a15ea766c730e5f6616e2fa8f
www6v <www6v@126.com>
2024-01-20 05:59:36 +0800

    add

--- gitCommitPublic.sh
+++ gitCommitPublic.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# project push
+git add .
+git commit -m "add"
+git push origin master
+
+# public push
 cd public
 rm -rf *
 
 cd ..
 hugo
 
+
 git add .
 git commit -m "add"
 git push origin master
-
